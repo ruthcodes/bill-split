34c38c32c87beb333740f578175188ec48efd5f2
Ruth Ashleigh <notruth@notruth.com>
2018-08-19 20:55:12 -0700

    Added tip functionality

--- src/TaxAndTip.js
+++ src/TaxAndTip.js
@@ -5,9 +5,9 @@ export default class TaxAndTip extends Component{
     return (
       <div className="userControls">
         <p>Add Tip:</p>
-        <button className="btn btn-primary">10%</button>
-        <button className="btn btn-primary">15%</button>
-        <button className="btn btn-primary">20%</button>
+        <button className="btn btn-primary" data-value={10} onClick={this.props.addTip}>10%</button>
+        <button className="btn btn-primary" data-value={15} onClick={this.props.addTip}>15%</button>
+        <button className="btn btn-primary" data-value={20} onClick={this.props.addTip}>20%</button>
       </div>
     )
   }
