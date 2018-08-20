3223a8df63a60a0b7c8a907ff8ec529ea234603f
Ruth Ashleigh <notruth@notruth.com>
2018-08-19 20:54:37 -0700

    Added tip functionality

--- src/Calculate.js
+++ src/Calculate.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 export default class Calculate extends Component{
+
+  outputTotal = () => {
+    let reduced = this.props.prices.reduce((a,b) => parseFloat(a) + parseFloat(b))
+    let tip = this.props.tip;
+    let addTip;
+    switch(tip) {
+      case "10":
+          addTip = 1.1
+          break;
+      case "15":
+          addTip = 1.15
+          break;
+      case "20":
+          addTip = 1.2
+          break;
+      default:
+          addTip = 1
+    }
+
+    return (parseFloat(reduced) * addTip).toFixed(2)
+  }
+
   render(){
     return (
       <div className="runningTotal">
-        <p>Total: </p> {this.props.prices.reduce((a,b) => a + b).toFixed(2)}
+        <p>{this.props.tip}% Tip</p>
+        <p>Total: </p> {this.outputTotal()}
       </div>
     )
   }
